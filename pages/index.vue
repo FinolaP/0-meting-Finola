910d3e4a09333625b31ffed93da3a21fb9c8e0e5
Finola <f.a.j.parkinson@students.uu.nl>
2022-06-07 18:06:50 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+         <h1>Finola's 0 meting</h1> 
+        </v-card-title>
+        <v-card-text>
+        <p>Om naar andere pagina's te gaan kun je het menu gebruiken. Je kunt ook via <nuxt-link to="/pagina2">deze link</nuxt-link> naar pagina 2 van de opdracht.  </p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/pagina2.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div>
+
+        <h2>Pagina 2</h2>
+
+
+        <body>
+
+            <section>
+                <article>
+                    <p>Om naar andere pagina's te gaan kun je het menu gebruiken. Via <nuxt-link to="/">deze link</nuxt-link> kun je naar de 'welcome' pagina.</p>
+                </article>
+            </section>
+        </body>
+        <div class="rocket" >
+            <img src="./../images/rocket.png" title="credits: image from Figma" class="align">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+ data: function() {
+ return {
+ message: "dit is een message"
+ }
+ }
+}
+</script>
+<style scoped>
+
+</style>
